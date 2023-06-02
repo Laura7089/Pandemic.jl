6c1cc5960075ccfcb3127bb3d8ba8948dac7a945
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2023-06-02 18:59:42 +0100

    refactor: swap stupid naming in tests

--- test/Pandemic.jl
+++ test/Pandemic.jl
@@ -11,44 +11,44 @@ world1 = begin
     Pandemic.finaliseworld(b)
 end
 
-game() = Game(
+testgame() = Game(
     world = world1,
     numplayers = 2,
     difficulty = Introductory,
 )
 
 @testset "cubesinplay" begin
-    testgame = game()
-    testgame.cubes = [0 3 0 0; 2 2 0 0]
+    game = testgame()
+    game.cubes = [0 3 0 0; 2 2 0 0]
 
-    @test Pandemic.cubesinplay(testgame, Pandemic.Blue) == 5
-    @test Pandemic.cubesinplay(testgame, Pandemic.Black) == 2
-    @test Pandemic.cubesinplay(testgame, Pandemic.Yellow) == 0
+    @test Pandemic.cubesinplay(game, Pandemic.Blue) == 5
+    @test Pandemic.cubesinplay(game, Pandemic.Black) == 2
+    @test Pandemic.cubesinplay(game, Pandemic.Yellow) == 0
 end
 
 @testset "checkstate" begin
-    testgame = game()
-    testgame.cubes = [Pandemic.CUBES_PER_DISEASE 0 0 0; 0 0 0 0]
-    @test Pandemic.checkstate!(testgame) == Pandemic.Lost
+    game = testgame()
+    game.cubes = [Pandemic.CUBES_PER_DISEASE 0 0 0; 0 0 0 0]
+    @test Pandemic.checkstate!(game) == Pandemic.Lost
 
-    testgame = game()
-    testgame.outbreaks = Pandemic.MAX_OUTBREAKS
-    @test Pandemic.checkstate!(testgame) == Pandemic.Lost
+    game = testgame()
+    game.outbreaks = Pandemic.MAX_OUTBREAKS
+    @test Pandemic.checkstate!(game) == Pandemic.Lost
 
-    testgame = game()
-    testgame.outbreaks = Pandemic.MAX_OUTBREAKS - 1
-    testgame.drawpile = collect(1:20)
-    @test Pandemic.checkstate!(testgame) == Pandemic.Playing
+    game = testgame()
+    game.outbreaks = Pandemic.MAX_OUTBREAKS - 1
+    game.drawpile = collect(1:20)
+    @test Pandemic.checkstate!(game) == Pandemic.Playing
 
-    testgame = game()
-    testgame.diseases = [Pandemic.Cured for _ in instances(Pandemic.Disease)]
-    @test Pandemic.checkstate!(testgame) == Pandemic.Won
+    game = testgame()
+    game.diseases = [Pandemic.Cured for _ in instances(Pandemic.Disease)]
+    @test Pandemic.checkstate!(game) == Pandemic.Won
 end
 
 @testset "infectcity!" begin
     begin
-        testgame = game()
-        Pandemic.infectcity!(testgame, city1)
+        game = testgame()
+        Pandemic.infectcity!(game, city1)
     end
 
     # TODO: *way* more different scenarios need testing for this
