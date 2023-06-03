ae781ca07513a886e39a25d5bc70ab1bc40ebc8b
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2023-06-03 11:10:19 +0100

    feat: add Formatting.summary, augment Formatting.players to show hands

--- src/Formatting.jl
+++ src/Formatting.jl
@@ -5,7 +5,7 @@ Human-readable string representations of various parts of the game.
 """
 module Formatting
 
-using Pandemic: Game, Disease, CUBE_CHARS
+using Pandemic
 
 """
     citycubes(game, c[; reprs])
@@ -58,11 +58,39 @@ end
 """
     players(game)
 
-Get a (multiline) [`String`](@ref) showing where each player is.
+Get a (multiline) [`String`](@ref) showing where each player is and a summary of their hand.
 """
 function players(game::Game)::String
+    function counthand(hand)
+        buckets = Dict([d => 0 for d in instances(Disease)])
+        for card in hand
+            buckets[game.world.cities[card].colour] += 1
+        end
+        filter!(p -> p.second > 0, buckets)
+        return join(buckets, ", ")
+    end
     city(p) = game.world.cities[game.playerlocs[p]].id
-    join(("♟️ $p: $(city(p))" for p = 1:game.numplayers), "\n")
+
+    join(("♟️ $p: $(city(p)), hand: $(counthand(game.hands[p]))" for p = 1:game.numplayers), "\n")
+end
+
+"""
+    summary(game)
+
+Get a simple human-readable summary of the state of `game`.
+"""
+function summary(game::Game)::String
+    cubes = ["$d: $(sum(game.cubes[:, Int(d)]))" for d in instances(Disease)]
+    stationlocs = [getcity(game.world, c)[2].id for c in stations(game)]
+    state = Pandemic.checkstate(game)
+
+    return """players:
+    $(players(game))
+    cubes: $(join(cubes, ", "))
+    stations: $(join(stationlocs, ", "))
+    draw cards: $(length(game.drawpile))
+    outbreaks: $(game.outbreaks)
+    state: $state"""
 end
 
 end
